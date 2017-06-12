9764c3ebea9548de5342ef7b915fa52f1abd9a9a
Jason Walsh <jwalsh@saylent.com>
2017-06-12 17:28:50 -0400

    Move to ES6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-exports._ = function(n) { console.log('mod-d v1', n); return n; }
+import * as a from '@jwalsh/mod-a'
+import * as b from '@jwalsh/mod-b'
+import * as c from '@jwalsh/mod-c'
+
+let _ = function(n) {
+
+
+  console.log('mod-d v1', n); return n;
+  console.log('Updated a to v2')
+
+  console.log(a._(1))
+  console.log(b._(2))
+  console.log(c._(3))
+
+  return n;
+};
+
+export { _ }
